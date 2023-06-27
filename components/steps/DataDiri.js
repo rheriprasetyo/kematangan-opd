6eb10d8ff9eb4d2e813727a04f68280bd992f660
Heri Prasetyo <r.heriprasetyo@gmail.com>
2023-06-27 09:17:37 +0800

    Update nama perangkat daerah

--- components/steps/DataDiri.js
+++ components/steps/DataDiri.js
@@ -58,43 +58,72 @@ function DataDiri({ data, setData }) {
               className="pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 z-1 focus:outline-none focus:ring-0 focus:border-black border-gray-200"
             >
               <option value="" selected disabled hidden></option>
-              <option value={"Setda"}>Setda</option>
-              <option value={"Setwan"}>Setwan</option>
+              <option value={"Setda"}>Sekretariat Daerah</option>
+              <option value={"Setwan"}>Sekretariat Dewan</option>
               <option value={"Inspektorat"}>Inspektorat</option>
               <option value={"Dinas Pendidikan"}>Dinas Pendidikan</option>
               <option value={"Dinas Kesehatan"}>Dinas Kesehatan</option>
-              <option value={"Dinas PUPR"}>Dinas PUPR</option>
-              <option value={"Dinas Perkim"}>Dinas Perkim</option>
-              <option value={"DPPPA"}>DPPPA</option>
-              <option value={"Dinas LH"}>Dinas LH</option>
-              <option value={"Dinas Dukcapil"}>Dinas Dukcapil</option>
-              <option value={"Dishub"}>Dishub</option>
-              <option value={"Diskominfo"}>Diskominfo</option>
-              <option value={"Dinas Perindustrian"}>Dinas Perindustrian</option>
+              <option value={"Dinas PUPR"}>
+                Dinas Pekerjaan Umum dan Perumahan Rakyat
+              </option>
+              <option value={"Dinas Perkim"}>
+                Dinas Perumahan dan Kawasan Permukiman
+              </option>
+              <option value={"DPPPA"}>
+                Dinas Pemberdayaan Perempuan dan Perlindungan Anak
+              </option>
+              <option value={"Dinas LH"}>Dinas Lingkungan Hidup</option>
+              <option value={"Dinas Dukcapil"}>
+                Dinas Kependudukan dan Catatan Sipil
+              </option>
+              <option value={"Dishub"}>Dinas Perhubungan</option>
+              <option value={"Diskominfo"}>
+                Dinas Komunikasi dan Informatika
+              </option>
+              <option value={"Dinas Perindustrian"}>
+                Dinas Perindustrian, Koperasi Usaha Kecil dan Menengah
+              </option>
               <option value={"Dinas Perdagangan"}>Dinas Perdagangan</option>
-              <option value={"DPMPTSP"}>DPMPTSP</option>
-              <option value={"Dinas Ketapang"}>Dinas Ketapang</option>
-              <option value={"Dispora"}>Dispora</option>
+              <option value={"DPMPTSP"}>
+                Dinas Penanaman Modal dan Pelayanan Terpadu Satu Pintu
+              </option>
+              <option value={"Dinas Ketapang"}>Dinas Ketahanan Pangan</option>
+              <option value={"Dispora"}>Dinas Kepemudaan dan Olah Raga</option>
               <option value={"Dinas Sosial"}>Dinas Sosial</option>
               <option value={"Dinas Tenaga Kerja"}>Dinas Tenaga Kerja</option>
-              <option value={"Damkar"}>Damkar</option>
+              <option value={"Damkar"}>Dinas Pemadam Kebakaran</option>
               <option value={"Dinas Pariwisata"}>Dinas Pariwisata</option>
-              <option value={"Diarpus"}>Diarpus</option>
+              <option value={"Diarpus"}>
+                Dinas Kearsipan dan Perpustakaan
+              </option>
               <option value={"Dinas Pertanian"}>Dinas Pertanian</option>
               <option value={"Dinas Perikanan"}>Dinas Perikanan</option>
-              <option value={"BAPPEDA"}>BAPPEDA</option>
-              <option value={"Balitbang"}>Balitbang</option>
-              <option value={"BKD"}>BKD</option>
-              <option value={"BKPSDM"}>BKPSDM</option>
-              <option value={"BPBD"}>BPBD</option>
-              <option value={"Bakesbangpol"}>Bakesbangpol</option>
-              <option value={"SatPol PP"}>SatPol PP</option>
-              <option value={"RSUD"}>RSUD</option>
+              <option value={"DP2KB"}>
+                Dinas Pengendalian Penduduk dan Keluarga Berencana
+              </option>
+              <option value={"BAPPEDA"}>
+                Badan Perencanaan Pembangunan Daerah
+              </option>
+              <option value={"Balitbang"}>
+                Badan Penelitian dan Pengembangan
+              </option>
+              <option value={"BKD"}>Badan Keuangan Daerah</option>
+              <option value={"BKPSDM"}>
+                Badan Kepegawaian dan Pengembangan SDM
+              </option>
+              <option value={"BPBD"}>
+                Badan Penanggulangan Bencana Daerah
+              </option>
+              <option value={"Bakesbangpol"}>
+                Badan Kesatuan Bangsa dan Politik Dalam Negeri
+              </option>
+              <option value={"SatPol PP"}>Satuan Polisi Pamong Praja</option>
+              <option value={"RSUD"}>Rumah Sakit Uumum Daerah</option>
               <option value={"Kec Mataram"}>Kecamatan Mataram</option>
               <option value={"Kec Selaparang"}>Kecamatan Selaparang</option>
               <option value={"Kec Cakranegara"}>Kecamatan Cakranegara</option>
               <option value={"Kec Sandubaya"}>Kecamatan Sandubaya</option>
-              <option value={"Kec Ampenan"}>Kecamatan Ampenenan</option>
+              <option value={"Kec Ampenan"}>Kecamatan Ampenan</option>
               <option value={"Kec Sekarbela"}>Kecamatan Sekarbela</option>
             </select>
 
